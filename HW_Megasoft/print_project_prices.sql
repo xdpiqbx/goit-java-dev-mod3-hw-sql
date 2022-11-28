c2825f3a7af889d7a9ce43c5a30ecb2b01b7bb58
xdpiqbx <xdpiqbx@gmail.com>
2022-11-28 07:19:06 +0200

    fix tab

--- HW_Megasoft/print_project_prices.sql
+++ HW_Megasoft/print_project_prices.sql
@@ -1,10 +1,10 @@
 SELECT
     CONCAT ('Project ', project_id) AS name,
-        DATEDIFF(
-                MONTH,
-                CAST (start_date AS DATE),
-                CAST(finish_date AS DATE)
-            ) *
+    DATEDIFF(
+        MONTH,
+        CAST (start_date AS DATE),
+        CAST(finish_date AS DATE)
+    ) *
     SUM(salary) AS price
     FROM project_worker
     INNER JOIN project ON project_worker.project_id = project.id
